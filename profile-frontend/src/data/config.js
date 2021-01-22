a41ab4475cbd8a8a754af61716b8872aa33a0c16
Abhishek Raj <mabhishek.tethered@gmail.com>
2021-01-22 15:35:16 +0000

    My first commit

---
+++ profile-frontend/src/data/config.js
@@ -0,0 +1,10 @@
+
+module.exports = {
+  // TODO: change your profile information here
+  name: "Abhishek Raj",
+  greeting: "Hey everyone 👋",
+  greetingDescription: "I'm Abhishek and I'm working to be  a Software Engineer!",
+  githubUrl: "https://github.com/AbhishekRaj0037",
+  linkedinUrl: "https://www.linkedin.com/in/abhishek-raj-365b5b202/",
+  cvLink: "",
+};
